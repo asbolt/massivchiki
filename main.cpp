91ab6535585cdd0b1baa022068e2a830dcf34b34
asbolt <asboltova@gmail.com>
2024-08-27 17:03:54 +0300

    norm versia

--- main.cpp
+++ main.cpp
@@ -9,38 +9,31 @@
 
 main ()
 {
-    double data [STRING][COMPONENTS];
+    double data [STRING][COMPONENTS] = {{0, 0, 0, 0, 0},
+                                        {1.2, 0, 0, 0, 0},
+                                        {2.6, 4.3, 0, 0, 0},
+                                        {2.3, 1.6, 3.5, 0, 0},
+                                        {3.4, 5.1, 1.1, 2.1, 0}};
 
     int y = 0;
     int x = 0;
-
-    for (y = 0; y < 5; y++)
-    {
-        for (x = 0; x < y; x++)
-        {
-            printf ("Введите счет игры %d:%d\n", y, x);
-            scanf ("%lg", &data[y][x]);
-        }
-    }
-
-    for (y = 0; y < 5; y++)
-    {
-        for (x = 4; x <= COMPONENTS - y; x--)
-        {
-            data[y][x] = 0;
-        }
-    }
 
     for (int i = 0; i < 5; i++)
     {
         printf ("%10d", i);
     }
+    printf ("\n");
 
     for (y = 0; y < 5; y++)
     {
-        for (x = 0; x < 5; x++)
+        printf ("%d", y);
+        for (x = 0; x < y; x++)
         {
-            printf ("%10lg", data[y][x]);
+            printf ("%10.1lf", data[y][x]);
+        }
+        for (x = y; x < 5; x++)
+        {
+            printf ("%10.0lf", data[y][x]);
         }
         printf ("\n");
     }
