e38e698525e6e73b1f6c0e391e2da191ecaafd28
asbolt <asboltova@gmail.com>
2024-08-28 15:43:40 +0300

    max ckazal sdelat tak

--- main.cpp
+++ main.cpp
@@ -9,14 +9,11 @@
 
 main ()
 {
-    double data [STRING][COMPONENTS] = {{0, 0, 0, 0, 0},
-                                        {1.2, 0, 0, 0, 0},
-                                        {2.6, 4.3, 0, 0, 0},
-                                        {2.3, 1.6, 3.5, 0, 0},
-                                        {3.4, 5.1, 1.1, 2.1, 0}};
+    double data [] = {1.2, 2.6, 4.3, 2.3, 1.6, 3.5, 3.4, 5.1, 1.1, 2.1};
 
     int y = 0;
     int x = 0;
+    
 
     for (int i = 0; i < 5; i++)
     {
@@ -27,14 +24,12 @@ main ()
     for (y = 0; y < 5; y++)
     {
         printf ("%d", y);
+
         for (x = 0; x < y; x++)
         {
-            printf ("%10.1lf", data[y][x]);
+            printf ("%10.1lf", data [(y-1)*y/2 + x]);
         }
-        for (x = y; x < 5; x++)
-        {
-            printf ("%10.0lf", data[y][x]);
-        }
+
         printf ("\n");
     }
 }
